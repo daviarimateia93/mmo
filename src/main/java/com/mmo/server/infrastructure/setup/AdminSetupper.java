ab43cf595f7d018089dbfbcb2e00b2e18709897e
daviarimateia93 <daviarimateia93@gmail.com>
2022-01-28 12:46:08 -0300

    add more admin users

--- src/main/java/com/mmo/server/infrastructure/setup/AdminSetupper.java
+++ src/main/java/com/mmo/server/infrastructure/setup/AdminSetupper.java
@@ -1,5 +1,8 @@
 package com.mmo.server.infrastructure.setup;
 
+import java.util.UUID;
+import java.util.stream.IntStream;
+
 import com.mmo.server.core.attribute.Attributes;
 import com.mmo.server.core.map.Position;
 import com.mmo.server.core.player.Player;
@@ -18,34 +21,36 @@ import lombok.ToString;
 @ToString
 public class AdminSetupper {
 
-    private static final String CONFIG_ADMIN_USER_ID = "admin.user.id";
-    private static final String CONFIG_ADMIN_USER_NAME = "admin.user.name";
-    private static final String CONFIG_ADMIN_USER_PASSWORD = "admin.user.password";
+    private static final String CONFIG_ADMIN_COUNT = "admin.count";
 
-    private static final String CONFIG_ADMIN_PLAYER_ID = "admin.player.id";
-    private static final String CONFIG_ADMIN_PLAYER_USER_ID = "admin.player.userId";
-    private static final String CONFIG_ADMIN_PLAYER_NAME = "admin.player.name";
-    private static final String CONFIG_ADMIN_PLAYER_POSITION_X = "admin.player.position.x";
-    private static final String CONFIG_ADMIN_PLAYER_POSITION_Z = "admin.player.position.z";
-    private static final String CONFIG_ADMIN_PLAYER_STATS_STRENGTH = "admin.player.stats.strength";
-    private static final String CONFIG_ADMIN_PLAYER_STATS_DEXTERITY = "admin.player.stats.dexterity";
-    private static final String CONFIG_ADMIN_PLAYER_STATS_INTELLIGENCE = "admin.player.stats.intelligence";
-    private static final String CONFIG_ADMIN_PLAYER_STATS_CONCENTRATION = "admin.player.stats.concentration";
-    private static final String CONFIG_ADMIN_PLAYER_STATS_SENSE = "admin.player.stats.sense";
-    private static final String CONFIG_ADMIN_PLAYER_STATS_CHARM = "admin.player.stats.charm";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP = "admin.player.attributes.hp";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP = "admin.player.attributes.mp";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK = "admin.player.attributes.attack";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_DEFENSE = "admin.player.attributes.defense";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MAGIC_DEFENSE = "admin.player.attributes.magicDefense";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_HIT_RATE = "admin.player.attributes.hitRate";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_CRITICAL = "admin.player.attributes.critical";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_DODGE_RATE = "admin.player.attributes.dodgeRate";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_SPEED = "admin.player.attributes.attackSpeed";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MOVE_SPEED = "admin.player.attributes.moveSpeed";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP_RECOVERY = "admin.player.attributes.hpRecovery";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP_RECOVERY = "admin.player.attributes.mpRecovery";
-    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_RANGE = "admin.player.attributes.attackRange";
+    private static final String CONFIG_ADMIN_USER_ID = "admin[%d].user.id";
+    private static final String CONFIG_ADMIN_USER_NAME = "admin[%d].user.name";
+    private static final String CONFIG_ADMIN_USER_PASSWORD = "admin[%d].user.password";
+
+    private static final String CONFIG_ADMIN_PLAYER_ID = "admin[%d].player.id";
+    private static final String CONFIG_ADMIN_PLAYER_USER_ID = "admin[%d].player.userId";
+    private static final String CONFIG_ADMIN_PLAYER_NAME = "admin[%d].player.name";
+    private static final String CONFIG_ADMIN_PLAYER_POSITION_X = "admin[%d].player.position.x";
+    private static final String CONFIG_ADMIN_PLAYER_POSITION_Z = "admin[%d].player.position.z";
+    private static final String CONFIG_ADMIN_PLAYER_STATS_STRENGTH = "admin[%d].player.stats.strength";
+    private static final String CONFIG_ADMIN_PLAYER_STATS_DEXTERITY = "admin[%d].player.stats.dexterity";
+    private static final String CONFIG_ADMIN_PLAYER_STATS_INTELLIGENCE = "admin[%d].player.stats.intelligence";
+    private static final String CONFIG_ADMIN_PLAYER_STATS_CONCENTRATION = "admin[%d].player.stats.concentration";
+    private static final String CONFIG_ADMIN_PLAYER_STATS_SENSE = "admin[%d].player.stats.sense";
+    private static final String CONFIG_ADMIN_PLAYER_STATS_CHARM = "admin[%d].player.stats.charm";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP = "admin[%d].player.attributes.hp";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP = "admin[%d].player.attributes.mp";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK = "admin[%d].player.attributes.attack";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_DEFENSE = "admin[%d].player.attributes.defense";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MAGIC_DEFENSE = "admin[%d].player.attributes.magicDefense";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_HIT_RATE = "admin[%d].player.attributes.hitRate";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_CRITICAL = "admin[%d].player.attributes.critical";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_DODGE_RATE = "admin[%d].player.attributes.dodgeRate";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_SPEED = "admin[%d].player.attributes.attackSpeed";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MOVE_SPEED = "admin[%d].player.attributes.moveSpeed";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP_RECOVERY = "admin[%d].player.attributes.hpRecovery";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP_RECOVERY = "admin[%d].player.attributes.mpRecovery";
+    private static final String CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_RANGE = "admin[%d].player.attributes.attackRange";
 
     private final UserRepository userRepository;
     private final PlayerRepository playerRepository;
@@ -62,50 +67,69 @@ public class AdminSetupper {
     }
 
     public void setup() {
-        userRepository.persist(newUser());
-        playerRepository.persist(newPlayer());
+        IntStream.range(1, configProvider.getInt(CONFIG_ADMIN_COUNT) + 1)
+                .forEach(index -> {
+                    userRepository.persist(newUser(index));
+                    playerRepository.persist(newPlayer(index));
+                });
     }
 
-    private User newUser() {
+    private User newUser(int index) {
         return User.builder()
-                .id(configProvider.getUUID(CONFIG_ADMIN_USER_ID))
-                .name(configProvider.getString(CONFIG_ADMIN_USER_NAME))
-                .password(configProvider.getString(CONFIG_ADMIN_USER_PASSWORD))
+                .id(getConfigUUID(CONFIG_ADMIN_USER_ID, index))
+                .name(getConfigString(CONFIG_ADMIN_USER_NAME, index))
+                .password(getConfigString(CONFIG_ADMIN_USER_PASSWORD, index))
                 .build();
     }
 
-    private Player newPlayer() {
+    private Player newPlayer(int index) {
         return Player.builder()
-                .instanceId(configProvider.getUUID(CONFIG_ADMIN_PLAYER_ID))
-                .userId(configProvider.getUUID(CONFIG_ADMIN_PLAYER_USER_ID))
-                .name(configProvider.getString(CONFIG_ADMIN_PLAYER_NAME))
+                .instanceId(getConfigUUID(CONFIG_ADMIN_PLAYER_ID, index))
+                .userId(getConfigUUID(CONFIG_ADMIN_PLAYER_USER_ID, index))
+                .name(getConfigString(CONFIG_ADMIN_PLAYER_NAME, index))
                 .position(Position.builder()
-                        .x(configProvider.getInt(CONFIG_ADMIN_PLAYER_POSITION_X))
-                        .z(configProvider.getInt(CONFIG_ADMIN_PLAYER_POSITION_Z))
+                        .x(getConfigInt(CONFIG_ADMIN_PLAYER_POSITION_X, index))
+                        .z(getConfigInt(CONFIG_ADMIN_PLAYER_POSITION_Z, index))
                         .build())
                 .stats(Stats.builder()
-                        .strength(configProvider.getInt(CONFIG_ADMIN_PLAYER_STATS_STRENGTH))
-                        .dexterity(configProvider.getInt(CONFIG_ADMIN_PLAYER_STATS_DEXTERITY))
-                        .intelligence(configProvider.getInt(CONFIG_ADMIN_PLAYER_STATS_INTELLIGENCE))
-                        .concentration(configProvider.getInt(CONFIG_ADMIN_PLAYER_STATS_CONCENTRATION))
-                        .sense(configProvider.getInt(CONFIG_ADMIN_PLAYER_STATS_SENSE))
-                        .charm(configProvider.getInt(CONFIG_ADMIN_PLAYER_STATS_CHARM))
+                        .strength(getConfigInt(CONFIG_ADMIN_PLAYER_STATS_STRENGTH, index))
+                        .dexterity(getConfigInt(CONFIG_ADMIN_PLAYER_STATS_DEXTERITY, index))
+                        .intelligence(getConfigInt(CONFIG_ADMIN_PLAYER_STATS_INTELLIGENCE, index))
+                        .concentration(getConfigInt(CONFIG_ADMIN_PLAYER_STATS_CONCENTRATION, index))
+                        .sense(getConfigInt(CONFIG_ADMIN_PLAYER_STATS_SENSE, index))
+                        .charm(getConfigInt(CONFIG_ADMIN_PLAYER_STATS_CHARM, index))
                         .build())
                 .attributes(Attributes.builder()
-                        .hp(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP))
-                        .mp(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP))
-                        .attack(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK))
-                        .defense(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_DEFENSE))
-                        .magicDefense(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MAGIC_DEFENSE))
-                        .hitRate(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_HIT_RATE))
-                        .critical(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_CRITICAL))
-                        .dodgeRate(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_DODGE_RATE))
-                        .attackSpeed(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_SPEED))
-                        .moveSpeed(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MOVE_SPEED))
-                        .hpRecovery(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP_RECOVERY))
-                        .mpRecovery(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP_RECOVERY))
-                        .attackRange(configProvider.getInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_RANGE))
+                        .hp(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP, index))
+                        .mp(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP, index))
+                        .attack(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK, index))
+                        .defense(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_DEFENSE, index))
+                        .magicDefense(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MAGIC_DEFENSE, index))
+                        .hitRate(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_HIT_RATE, index))
+                        .critical(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_CRITICAL, index))
+                        .dodgeRate(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_DODGE_RATE, index))
+                        .attackSpeed(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_SPEED, index))
+                        .moveSpeed(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MOVE_SPEED, index))
+                        .hpRecovery(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_HP_RECOVERY, index))
+                        .mpRecovery(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_MP_RECOVERY, index))
+                        .attackRange(getConfigInt(CONFIG_ADMIN_PLAYER_ATTRIBUTES_ATTACK_RANGE, index))
                         .build())
                 .build();
+    }
+
+    private String getConfigString(String format, int index) {
+        return configProvider.getString(resolveConfigName(format, index));
+    }
+
+    private UUID getConfigUUID(String format, int index) {
+        return configProvider.getUUID(resolveConfigName(format, index));
+    }
+
+    private int getConfigInt(String format, int index) {
+        return configProvider.getInt(resolveConfigName(format, index));
+    }
+
+    private String resolveConfigName(String format, Object... arguments) {
+        return String.format(format, arguments);
     }
 }

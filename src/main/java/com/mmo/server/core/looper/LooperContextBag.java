9c48af6aa51722380e522c5d82c5493c8c127a23
daviarimateia93 <daviarimateia93@gmail.com>
2022-01-30 10:34:44 -0300

    change looper context bag visibility

--- src/main/java/com/mmo/server/core/looper/LooperContextBag.java
+++ src/main/java/com/mmo/server/core/looper/LooperContextBag.java
@@ -1,22 +1,11 @@
 package com.mmo.server.core.looper;
 
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@Getter
-@Setter(AccessLevel.PROTECTED)
-@EqualsAndHashCode
-@ToString
+@Data
 public class LooperContextBag implements LooperContext {
 
     private Long tick;
     private Long lag;
     private Double desiredLag;
-
-    protected LooperContextBag() {
-
-    }
 }
